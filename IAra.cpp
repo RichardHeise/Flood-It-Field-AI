2b4a1ba9070ce00ef2e50d8c6d7f3e071e9b613f
Richard Heise <rfhf19@inf.ufpr.br>
2021-08-08 03:14:48 -0300

    comentários para terminar

--- IAra.cpp
+++ IAra.cpp
@@ -1,18 +1,32 @@
+/**
+ * @file  IAra.h
+ * @brief Arquivo com os cabeçalhos das funções da inteligência artificial.
+ * @date 27/02/2021
+ * @author Richard Fernando Heise Ferreira (GRR20191053)
+ **/
+
 #include "IAra.h"
 
 vector<char> possiveisCores (matriz m) {
     
+    // As bordas de nosso cluster são as jogadas possiveis.
     vector<char> bordas;
+
     int controle = 0;
+
+    // Floodamos de 0s nosso cluster para não o considerarmos.
     floodFill(&m, 0, make_pair(0,0));
 
+    // Percorremos o cluster
     for (int i = 0; i < m.size(); i++) {
         for (int j = 0; j < m[0].size(); j++) {
 
+            // Se não estamos no cluster, então estamos em uma borda.
             if (m[i][j] != m[0][0]) {
 
+                //  Resetamos o controle
                 if (j-1 >= 0 && m[i][j-1] == 0) {
-
+                    
                     bordas.push_back(m[i][j]); 
                     controle = 0;
 
@@ -25,15 +39,18 @@ vector<char> possiveisCores (matriz m) {
 
                 controle++;
             } 
-        }
-        
+        }   
+        // Se controle não foi resetado, então percorremos uma linha inteira
+        // sem bordas, portanto, não devemos continuar buscando bordas.
         if (controle == m[0].size() ) break;
     }
 
+    // Ordenar para depois retirar as duplicatas.
     sort(bordas.begin(), bordas.end(), [](auto &left, auto &right) {
         return left < right;
     });
 
+    //Retira duplicatas.
     vector<char>::iterator it;
     it = unique(bordas.begin(), bordas.begin() + bordas.size());
 
@@ -44,18 +61,28 @@ vector<char> possiveisCores (matriz m) {
 
 float h(matriz m, int cores) {
 
+
     int regioes = 0;
+    // matriz de distâncias.
     matriz distancias = m;
 
+    //  Floodamos nosso cluster com 0s para não o considerarmos.
     floodFill(&distancias, 0, make_pair(0,0));
 
     int maiorDist = 0;
 
+    // Percorremos o tabuleiro.
     for (int i = 0; i < m.size(); i++) {
         for (int j = 0; j < m[0].size(); j++) {
 
+            //=========================================================================================================
+            //======================================= HEURÍSTICA 1 (PRINCIPAL) ========================================
+            //========================================================================================================>
+
+            // Calculamos as distâncias de todos os pontos do tabuleiro referente ao nosso cluster.
             if (distancias[i][j] != 0) {
 
+                // A distância é sempre o mínimo somado de 1 entre o valor que está à esquerda e acima de cada elemento. 
                 if (i-1 >= 0 && i-1 < distancias.size() && j-1 >= 0 && j-1 < distancias[0].size()) {
                     distancias[i][j] = min(distancias[i-1][j], distancias[i][j-1]) + 1; 
 
@@ -66,11 +93,18 @@ float h(matriz m, int cores) {
                     distancias[i][j] = distancias[i][j-1] + 1; 
                 } 
 
+                // Guardamos a maior distância.
                 if (distancias[i][j] > maiorDist)
                     maiorDist = distancias[i][j];
             }
+            //<=======================================================================================================
+
+            //==========================================================================================================
+            //======================================= HEURÍSTICA 2 (SECUNDÁRIA) ========================================
+            //=========================================================================================================>
 
             if (m[i][j] != 0) {
+                // Literalmente o FloodFill() na matriz de cópia dos parametros para evitar contar a mesma região.
                 vector<coordenada> lista;
 
                 lista.push_back(make_pair(i,j));
@@ -90,27 +124,41 @@ float h(matriz m, int cores) {
                     }
                 }
 
+                // Contamos quantas regiões temos ao total.
                 regioes++;
             }
+            //<========================================================================================================
         }
     }
 
+    // Retornamos a soma das regiões divididos pelo número de cores (que nos dá uma aproximação das jogadas totais)
+    // com a jogada que diminui a distância. Quanto menor esse score, melhor.
 
     return (((float) regioes / (float) cores) + (float)maiorDist);
 }
 
-float preveJogada (matriz m, int cores, int max_escopos) {
+float preveJogada (matriz m, int cores, int maxEscopos) {
 
+    // Vetor que relaciona cada cor a uma heurística.
     vector<pair<char, float>> controle;
+
+    // Matriz temporária.
     matriz temp_m = m;
 
+    // Se a matriz vier resolvida, então a cor que a resolveu é a melhor jogada.
+    // Logo, retornamos o menor valor heurístico possível.
     if ( resolveu(temp_m) ) return 0;
 
-    for (int escopos = 0; escopos < max_escopos; escopos++) {
+    // Repetição do laço para cada tabuleiro.
+    for (int escopos = 0; escopos < maxEscopos; escopos++) {
 
+        // Se resolvemos, saia do laço, pois achamos a melhor cor.
         if ( resolveu(temp_m) ) break;
+
+        // Limpamos controle a cada nova previsão.
         controle.clear();
 
+        // Possíveis cores adjacentes.
         vector<char> coresBorda = possiveisCores(temp_m); 
 
         for (char cor : coresBorda) {
@@ -120,45 +168,67 @@ float preveJogada (matriz m, int cores, int max_escopos) {
                 
                 floodFill(&aux, cor, make_pair(0,0));
 
-                if ( resolveu(temp_m) ) break;
-
+                // Mais de uma previsão pode estar correta, porém, queremos a que está correta
+                // antes das demais, portanto, somamos à heurística em qual profundidade encontramos a solução.
                 controle.push_back(make_pair(cor, (float)escopos + h(aux, cores)));
             }
         }
 
+        // Ordenamos o vetor pela melhor jogada.
         sort(controle.begin(), controle.end(), [](auto &left, auto &right) {
             return left.second < right.second;
         });
 
+        // Jogamos a melhor jogada decidida anteriormente.
         floodFill(&temp_m, controle.front().first, make_pair(0,0));
     }
     
-
+    //Retornamos a heuristica da melhor jogada;
     return controle.front().second;
 }   
 
 char buscaMelhorJogada (matriz m, int cores) {
 
+    // vetor de pares que relaciona uma cor a uma heurística.
     vector<pair<char, float>> controle;
+
+    // Cores adjacentes ao cluster.
     vector<char> coresBorda = possiveisCores(m);
+
+    /*                      IMPORTANTE
+    /* Essa equação foi obtida com base em testes:
+    /* defini que, com 20 cores em um tabuleiro 100x100
+    /* seria impossível prever jogadas no futuro dentro de 2 minutos.
+    /* Já com 19, poderia prever uma jogada no futuro. Com 14, duas.
+    /* Portanto, tendo três pontos {(20,0); (19,1); (14,2)} gerei um gráfico
+    /* queresultou nessa função. Ela só vale 0 para 20, que é quando não prevemos,
+    /* e sim usamos a heurística diretamente. 
+    /* **Portanto, prevemos o número de jogadas que devemos prever para cada jogada possível.**
+    */
 
     int previsoes = (-4.796l*log(coresBorda.size()) + 14.715);
 
+    // Se devemos prever, prevemos. Senão, não prevemos.
     if (previsoes) {
+        // Para cada cor na borda.
         for (char cor : coresBorda) {
             if (cor != m[0][0]) {
                 
                 float heuristica;
 
                 matriz temp_m = m;
+                // Floodamos uma matriz temporária com a cor.
                 floodFill(&temp_m, cor, make_pair(0,0));
 
+                // Chamamos nossa heurística com previsão para dada matriz pintada com cor.
                 heuristica = preveJogada(temp_m, cores, previsoes);
                 
+                // Guardamos o valor que nossa cor atingiu.
                 controle.push_back(make_pair(cor, heuristica));
             }
         }        
     } else {
+        // Mesmo do de cima.
         for (char cor : coresBorda) {
             if (cor != m[0][0]) {
                 
@@ -167,6 +237,7 @@ char buscaMelhorJogada (matriz m, int cores) {
                 matriz temp_m = m;
                 floodFill(&temp_m, cor, make_pair(0,0));
 
+                // Aqui, contudo, chamamos h diretamente, pois não queremos prever devido ao custo.
                 heuristica = h(temp_m, cores);
                 
                 controle.push_back(make_pair(cor, heuristica));
@@ -174,19 +245,20 @@ char buscaMelhorJogada (matriz m, int cores) {
         }
     }
 
+    // Ordenamos o vetor, porque é mais eficiente do que guardar o menos para cada iteração acima.
     sort(controle.begin(), controle.end(), [](auto &left, auto &right) {
         return left.second < right.second;
     });
 
+    // Peguemos a cor com menor heurística, guardada na frente do vetor.
     return controle.front().first;        
 }
-
 
 void resolve (matriz m, int cores) {
 
     vector<char> jogadas;
 
-
+    // Enquanto não resolvemos, buscamos a melhor jogada, a jogamos e a guardamos.
     while ( !resolveu(m) ) {
         
         char melhorJogada = buscaMelhorJogada(m, cores);
@@ -195,6 +267,7 @@ void resolve (matriz m, int cores) {
        
     }
 
+    // Coloca na tela o tamanho e a seuquência de boas jogadas.
     printf("%lu\n", jogadas.size());
     for (int i = 0; i < jogadas.size(); i++)
         printf("%d ", jogadas[i]);
